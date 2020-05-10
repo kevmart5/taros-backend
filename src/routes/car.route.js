767603c14b3fd2ce67d6218f8b824456da4c0645
Kevin Martinez <kevin.martinez@dnamic.ai>
2020-05-10 12:49:15 -0600

    fix error in route

--- src/routes/car.route.js
+++ src/routes/car.route.js
@@ -1,7 +1,7 @@
 module.exports = (app) => {
 	const car = require('../controllers/car.controller.js');
 
-	app('/', car.home);
+	app.get('/', car.home);
 	app.get('/car', car.findAll);
 	app.get('/car/:id', car.findById);
 };
