86d33bcc234afb95504bb08099f4ea2ee3c77f10
Kevin Martinez <kevin.martinez@dnamic.ai>
2020-05-17 16:28:44 -0600

    AS-36: Add new enpoint for car delete action

--- src/constants/errorMessages.js
+++ src/constants/errorMessages.js
@@ -1,3 +1,4 @@
 module.exports = {
-	CAR_REGISTER_ALREADY: 'Ya existe un carro registrado con esta placa',
+	CAR_REGISTER_ALREADY: 'Ya existe un vehículo registrado con esta placa',
+	CAR_DELETE_REQUEST: 'Algo salió mal eliminando el vehículo',
 };
